f97b9527e5c30cb9c487e8cdbce567c1f1e131cb
Bjorn Lind <bjorn_92@yahoo.se>
2020-07-29 22:55:53 +0200

    Initial project commit

---
+++ MathUtilities/SimpleMaths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtilities
+{
+    public class SimpleMaths
+    {
+        public static double Square(double val)
+        {
+            return val * val;
+        }
+    }
+}
